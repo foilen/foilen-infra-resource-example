d49fafbd1972eb8d5e5d8ad253630a389fd32374
Simon Levesque <simon@foilen.com>
2021-02-03 21:24:05 -0500

    CLOUD-131 Can search on any field

--- src/main/java/com/foilen/infra/resource/example/ExamplePluginDefinitionProvider.java
+++ src/main/java/com/foilen/infra/resource/example/ExamplePluginDefinitionProvider.java
@@ -29,48 +29,19 @@ public class ExamplePluginDefinitionProvider implements IPPluginDefinitionProvid
         IPPluginDefinitionV1 pluginDefinitionV1 = new IPPluginDefinitionV1("Infra", "Example", "An example of a plugin", "1.0.0");
 
         pluginDefinitionV1.addCustomResource(Ex1Resource.class, "Exemple 1", //
-                Arrays.asList("name"), //
-                Arrays.asList("year"));
+                Arrays.asList("name"));
         pluginDefinitionV1.addCustomResource(Ex2Resource.class, "Exemple 2", //
-                Arrays.asList("id"), //
-                Arrays.asList("year"));
+                Arrays.asList("id"));
         pluginDefinitionV1.addCustomResource(EmployeeResource.class, "Employee", //
-                Arrays.asList("firstName", "lastName"), //
-                Arrays.asList("birthday"));
+                Arrays.asList("firstName", "lastName"));
         pluginDefinitionV1.addCustomResource(JunitResource.class, "Junit", //
-                Arrays.asList(JunitResource.PROPERTY_TEXT, JunitResource.PROPERTY_ENUMERATION, JunitResource.PROPERTY_INTEGER_NUMBER), //
-                Arrays.asList( //
-                        JunitResource.PROPERTY_BOOL, //
-                        JunitResource.PROPERTY_DATE, //
-                        JunitResource.PROPERTY_DOUBLE_NUMBER, //
-                        JunitResource.PROPERTY_ENUMERATION, //
-                        JunitResource.PROPERTY_FLOAT_NUMBER, //
-                        JunitResource.PROPERTY_INTEGER_NUMBER, //
-                        JunitResource.PROPERTY_LONG_NUMBER, //
-                        JunitResource.PROPERTY_TEXT, //
-                        JunitResource.PROPERTY_SET_TEXTS, //
-                        JunitResource.PROPERTY_SET_DATES, //
-                        JunitResource.PROPERTY_SET_DOUBLES, //
-                        JunitResource.PROPERTY_SET_ENUMERATIONS, //
-                        JunitResource.PROPERTY_SET_LONGS, //
-                        JunitResource.PROPERTY_SET_INTEGERS, //
-                        JunitResource.PROPERTY_SET_FLOATS //
-                ));
+                Arrays.asList(JunitResource.PROPERTY_TEXT, JunitResource.PROPERTY_ENUMERATION, JunitResource.PROPERTY_INTEGER_NUMBER) //
+        );
         pluginDefinitionV1.addCustomResource(ConcreteLevel1.class, "Concrete Level 1", //
-                Arrays.asList(AbstractParent.PROPERTY_NAME), //
-                Arrays.asList( //
-                        AbstractParent.PROPERTY_NAME, //
-                        AbstractParent.PROPERTY_ON_PARENT, //
-                        ConcreteLevel1.PROPERTY_ON_LEVEL_1 //
-                ));
+                Arrays.asList(AbstractParent.PROPERTY_NAME) //
+        );
         pluginDefinitionV1.addCustomResource(ConcreteLevel2.class, "Concrete Level 2", //
-                Arrays.asList(AbstractParent.PROPERTY_NAME), //
-                Arrays.asList( //
-                        AbstractParent.PROPERTY_NAME, //
-                        AbstractParent.PROPERTY_ON_PARENT, //
-                        ConcreteLevel1.PROPERTY_ON_LEVEL_1, //
-                        ConcreteLevel2.PROPERTY_ON_LEVEL_2 //
-                ));
+                Arrays.asList(AbstractParent.PROPERTY_NAME));
 
         pluginDefinitionV1.addTranslations("/com/foilen/infra/resource/example/messages");
 
